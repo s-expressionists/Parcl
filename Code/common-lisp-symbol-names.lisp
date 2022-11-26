977935c2c92c6eac47c9da25d42d4c2f077a4044
Robert Strandh <robert.strandh@gmail.com>
2022-11-26 09:04:02 +0100

    Improve grammar of LOOP clause.

--- Code/common-lisp-symbol-names.lisp
+++ Code/common-lisp-symbol-names.lisp
@@ -2,7 +2,7 @@
 
 (defun create-length-categories ()
   (loop with categories = (make-array 39 :initial-element '())
-        for symbol being the external-symbols
+        for symbol being each external-symbol
           of (cl:find-package "COMMON-LISP")
         for name = (symbol-name symbol)
         do (push name (aref categories (length name)))
