df794d6b3a857acaccb166ae264482326ec95ac3
Robert Strandh <robert.strandh@gmail.com>
2022-12-17 17:18:25 +0100

    Define accessor NICKNAMES and (SETF NICKNAMES).

--- Code/accessors.lisp
+++ Code/accessors.lisp
@@ -14,5 +14,26 @@
 ;;; This function is used to implement the standard function
 ;;; RENAME-PACKAGE.  The PACKAGE argument of this function must be a
 ;;; package object.  This function does not check whether NEW-NAME is
-;;; the name of an existing package.
+;;; the name or the nickname of an existing package.  The return value
+;;; of this function is NEW-NAME as required by the Common Lisp
+;;; standard.
 (defgeneric (setf name) (new-name client package))
+
+;;; This function is used to implement the standard function
+;;; PACKAGE-NICKNAMES.  Contrary to the Common Lisp standard function,
+;;; the PACKAGE argument of this function must be a package object,
+;;; whereas the standard function takes a package designator.  The
+;;; return value is a list of nicknames of PACKAGE.  This list is not
+;;; freshly allocated, so if client code wants a fresh list, it must
+;;; copy what this function returns.
+(defgeneric nicknames (client package))
+
+;;; This function is used to implement the standard function
+;;; RENAME-PACKAGE.  The PACKAGE argument of this function must be a
+;;; package object.  NEW-NICKNAMES is a list of strings that become
+;;; the new list of nicknames of PACKAGE.  This function does not
+;;; check whether NEW-NICKNAMES contains a string that is already the
+;;; name or the nickname of any existing package.  The return value of
+;;; this function is NEW-NICKNAMES as required by the Common Lisp
+;;; standard.
+(defgeneric (setf name) (new-nicknames client package))
