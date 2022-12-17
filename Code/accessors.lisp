a76e69063b7e6a394586ae54b83aba3b85ac57d7
Robert Strandh <robert.strandh@gmail.com>
2022-12-17 17:07:57 +0100

    Define accessor NAME.

--- Code/accessors.lisp
+++ Code/accessors.lisp
@@ -1,1 +1,12 @@
 (cl:in-package #:parcl)
+
+;;;; This file contains essential accessors for package objects.  They
+;;;; are deliberately distinct from any standard Common Lisp function,
+;;;; because we might want different implementations of these
+;;;; accessors in different contexts.
+
+;;; This function is used to implement the standard function
+;;; PACKAGE-NAME.  Contrary to the Common Lisp standard function, the
+;;; PACKAGE argument of this function must be a package object,
+;;; whereas the standard function takes a package designator.
+(defgeneric name (client package))
