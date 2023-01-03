3dcc99ca86497413282dda5125c60f820820d6e0
Robert Strandh <robert.strandh@gmail.com>
2023-01-03 08:40:37 +0100

    Introduce new slots and rename existing ones.  Add comments.

--- Code/Class/package-defclass.lisp
+++ Code/Class/package-defclass.lisp
@@ -20,9 +20,37 @@
 
 (defgeneric (setf used-by-list) (new-used-by-list package))
 
-(defgeneric external-symbols (package))
+;;; There can be a large number of external and internal symbols.
+;;; However, we think that the most frequent operation is FIND-SYMBOL,
+;;; and perhaps also INTERN.  As a result, we use a dual
+;;; representation for each of these sets of symbols, namely a hash
+;;; table to be used by FIND-SYMBOL and a simple list of all symbols
+;;; to be used by iterators and LOOP clauses.  When we need to remove
+;;; a symbol from one of the sets, we must then traverse the list, but
+;;; we think this operation is infrequent.
 
-(defgeneric internal-symbols (package))
+;;; Return the hash table holding external symbols.  The name of the
+;;; symbol is the key.
+(defgeneric external-symbols-table (package))
+
+;;; Return the list of external symbols.
+(defgeneric external-symbols-list (package))
+
+;;; Set the list of external symbols.
+(defgeneric (setf external-symbols-list) (new-symbols-list package))
+
+;;; Return the hash table holding internal symbols.  The name of the
+;;; symbol is the key.
+(defgeneric internal-symbols-table (package))
+
+;;; Return the list of internal symbols.
+(defgeneric internal-symbols-list (package))
+
+;;; Set the list of internal symbols.
+(defgeneric (setf internal-symbols-list) (new-symbols-list package))
+
+;;; We represent the shadowing symbols as a simple list, assuming that
+;;; in general there won't be very many of them.
 
 (defgeneric shadowing-symbols (package))
 
@@ -48,12 +76,18 @@
     :initarg :used-by-list
     :initform '()
     :accessor used-by-list)
-   (%external-symbols
+   (%external-symbols-table
     :initform (make-hash-table :test #'equal)
     :reader external-symbols)
-   (%internal-symbols
+   (%external-symbols-list
+    :initform '()
+    :accessor external-symbols-list)
+   (%internal-symbols-table
     :initform (make-hash-table :test #'equal)
     :reader internal-symbols)
+   (%internal-symbols-list
+    :initform '()
+    :accessor internal-symbols-list)
    (%shadowing-symbols
     :initarg :shadowing-symbols
     :initform '()
