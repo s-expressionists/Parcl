690886b2a18df8487e68eeae45907d7680ae818a
Robert Strandh <robert.strandh@gmail.com>
2023-01-05 10:00:21 +0100

    Improve the technique for removing an external or internal symbol.

--- Code/Class/methods.lisp
+++ Code/Class/methods.lisp
@@ -44,15 +44,38 @@
   (setf (gethash (symbol-name symbol) (external-symbols-table package))
         (first (external-symbols-list package))))
 
+;;; The technique we use for removing a symbol from the list works
+;;; like this: The hash table contains the CONS cells of the list.
+;;; Let us say that C1 is the cell containing the symbol, say S1, to
+;;; be removed.  If the there is a cell, say C2 after C1 in the list
+;;; containing the symbols S2, we move S2 to C1, and unlink C2 from
+;;; the list.  We update the hash table so that the entry for S2 now
+;;; points to C1.  If C1 is the last cell in the list, there are two
+;;; cases.  Case 1 is when C1 is the only cell in the list.  We then
+;;; just set the list of symbols to the empty list.  Case 2 is when
+;;; there is at least one cell in addition to C1 in the list.  Then,
+;;; in particular, the first cell in the list can not be C1, since C1
+;;; is the last one in the list.  Call the first cell in the list C2,
+;;; and the symbol therein S2.  We again move S2 to C1, and unlink C2
+;;; from the list by simply popping off the first element.  We update
+;;; the hash table so that the entry for S2 now points to C1.  In all
+;;; cases, we end by removing the entry for S1 from the hash table.
+
 (defmethod parcl:remove-external-symbol ((client client) package symbol)
   (let* ((table (external-symbols-table package))
          (cell (gethash (symbol-name symbol) table)))
     (if (null (rest cell))
-        (setf (external-symbol-list package)
-              (nbutlast (external-symbol-list package)))
+        (if (null (rest (external-symbol-list package)))
+            ;; There is only this one symbol left.
+            (setf (external-symbol-list package) '())
+            ;; Otherwise, we move the symbol from the first cell on
+            ;; the list to this cell.
+            (let ((symbol-to-move (first (external-symbol-list package))))
+              (setf (first cell) symbol-to-move)
+              (setf (gethash (symbol-name symbol-to-move) table) cell)
+              (pop (first (external-symbol-list package)))))
         (let ((next-symbol (second cell)))
-          (setf (gethash (symbol-name next-symbol) table)
-                cell)
+          (setf (gethash (symbol-name next-symbol) table) cell)
           (setf (rest cell) (rest (rest cell)))))
     (remhash (symbol-name symbol) table)))
 
@@ -71,11 +94,17 @@
   (let* ((table (internal-symbols-table package))
          (cell (gethash (symbol-name symbol) table)))
     (if (null (rest cell))
-        (setf (internal-symbol-list package)
-              (nbutlast (internal-symbol-list package)))
+        (if (null (rest (internal-symbol-list package)))
+            ;; There is only this one symbol left.
+            (setf (internal-symbol-list package) '())
+            ;; Otherwise, we move the symbol from the first cell on
+            ;; the list to this cell.
+            (let ((symbol-to-move (first (internal-symbol-list package))))
+              (setf (first cell) symbol-to-move)
+              (setf (gethash (symbol-name symbol-to-move) table) cell)
+              (pop (first (internal-symbol-list package)))))
         (let ((next-symbol (second cell)))
-          (setf (gethash (symbol-name next-symbol) table)
-                cell)
+          (setf (gethash (symbol-name next-symbol) table) cell)
           (setf (rest cell) (rest (rest cell)))))
     (remhash (symbol-name symbol) table)))
 
