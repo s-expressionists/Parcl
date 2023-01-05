60e104dccb81e16e1eba50248ae34146c1f101dd
Robert Strandh <robert.strandh@gmail.com>
2023-01-05 16:09:18 +0100

    Define generic function IMPORT.

--- Code/generic-functions.lisp
+++ Code/generic-functions.lisp
@@ -3,3 +3,10 @@
 ;;; This function can be used to implement the standard function
 ;;; FIND-SYMBOL.
 (defgeneric find-symbol (client package name))
+
+;;; This function can be used to implement the standard function
+;;; IMPORT.  If, as a result of a call to this function, SYMBOL is
+;;; imported into PACKAGE, and SYMBOL has no home packge then client
+;;; code is responsible for setting the home package of SYMBOL to
+;;; PACKAGE.
+(defgeneric import (client package symbol))
