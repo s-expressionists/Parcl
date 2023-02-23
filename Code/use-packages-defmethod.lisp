d23918bc21d2aebf4572833eecd79bc0dbccd7b8
Robert Strandh <robert.strandh@gmail.com>
2023-02-23 09:57:00 +0100

    Remove redundant word in comment.

--- Code/use-packages-defmethod.lisp
+++ Code/use-packages-defmethod.lisp
@@ -1,10 +1,10 @@
 (cl:in-package #:parcl)
 
 (defmethod use-packages (client package packages-to-use)
-  (let (;; The keys of hash table are symbol names.  The value of a
-        ;; key is is a list with one element for each distinct symbol
-        ;; with the key as a name.  Each element is alist where the
-        ;; CAR is the symbol, and the CDR is a list of packages for
-        ;; which there might be a confligt.
+  (let (;; The keys of hash table are symbol names. The value of a key
+        ;; is a list with one element for each distinct symbol with the
+        ;; key as a name. Each element is alist where the CAR is the
+        ;; symbol, and the CDR is a list of packages for which there
+        ;; might be a confligt.
         (conflicts (make-hash-table :test #'equal)))
     nil))
