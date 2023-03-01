7afde976b41ef7bff50ccd45b46716eeae479a37
Robert Strandh <robert.strandh@gmail.com>
2023-03-01 16:03:51 +0100

    Define function RESOLVE-CONFLICT.

--- Code/export-defmethod.lisp
+++ Code/export-defmethod.lisp
@@ -1,7 +1,18 @@
 (cl:in-package #:parcl)
 
-(define-condition export-conflict ()
-  nil)
+(defun resolve-conflict (symbols package)
+  (restart-case (error 'symbol-conflict
+                       :conflicting-symbols symbols
+                       :package package)
+    (choose-a-symbol ()
+      :report
+      (lambda (stream)
+        (format stream "Choose a symbol"))
+      :interactive
+      (lambda ()
+        (format *debug-io* "Your choice: ")
+        (return-from resolve-conflict
+                     (read *debug-io*))))))
 
 (defmethod export (client package symbol)
   nil)
