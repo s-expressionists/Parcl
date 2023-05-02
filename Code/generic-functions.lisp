d1ce6a825908b780a1059a99352f2368395023f8
Robert Strandh <robert.strandh@gmail.com>
2023-05-02 14:04:09 +0200

    Fix typo and improve comment.

--- Code/generic-functions.lisp
+++ Code/generic-functions.lisp
@@ -16,8 +16,11 @@
 (defgeneric use-packages (client package packages-to-use))
 
 ;;; This function can be used to implement the standard function
-;;; UNUSE-PACKAGE.
-(defgeneric unuse-packages (client package packages-to-unuse))
+;;; UNUSE-PACKAGE.  It differs from the standard function in that it
+;;; takes a single package to unuse as opposed to a list of packages
+;;; to unuse. Client code for the standard function must then call
+;;; this function multiple times.
+(defgeneric unuse-package (client package packages-to-unuse))
 
 ;;; This function can be used to implement the standard function EXPORT.
 ;;; It differs from the standard function in that it takes a single
