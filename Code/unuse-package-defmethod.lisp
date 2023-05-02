e1a12c0903fc26b0272dc5d685cb39ec337de107
Robert Strandh <robert.strandh@gmail.com>
2023-05-02 10:35:08 +0200

    Supply missing CLIENT argument.

--- Code/unuse-package-defmethod.lisp
+++ Code/unuse-package-defmethod.lisp
@@ -1,10 +1,10 @@
 (cl:in-package #:parcl)
 
 (defmethod unuse-package (client package package-to-unuse)
-  (unless (member package-to-unuse (use-list package)
+  (unless (member package-to-unuse (use-list client package)
                   :test #'eq)
     (error 'package-is-not-used
            :package package
            :package-to-unuse package-to-unuse))
-  (setf (use-list package)
-        (delete package-to-unuse (use-list package))))
+  (setf (use-list client package)
+        (delete package-to-unuse (use-list client package))))
