4ac80183f9f666d78d8bb81a6e83a7303531206a
Robert Strandh <robert.strandh@gmail.com>
2023-06-09 15:20:01 +0200

    Add local nicknames for Iconoclast and s-expression-syntax.

--- Code/packages-extrinsic.lisp
+++ Code/packages-extrinsic.lisp
@@ -2,5 +2,8 @@
 
 (defpackage parcl
   (:use #:common-lisp)
+  (:local-nicknames (#:ico #:iconoclast)
+                    (#:bld #:iconoclast-builder)
+                    (#:ses #:s-expression-syntax))
   (:shadow . #.parcl-asdf:*string-designators*)
   (:export . #.parcl-asdf:*string-designators*))
--- Code/packages-intrinsic.lisp
+++ Code/packages-intrinsic.lisp
@@ -2,4 +2,7 @@
 
 (defpackage parcl
   (:use #:common-lisp)
+  (:local-nicknames (#:ico #:iconoclast)
+                    (#:bld #:iconoclast-builder)
+                    (#:ses #:s-expression-syntax))
   (:export . #.parcl-asdf:*string-designators*))
