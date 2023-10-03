3151c79691ef719dcc36513d748e881e965ecaa9
Robert Strandh <robert.strandh@gmail.com>
2023-10-03 07:36:57 +0200

    Define generic functions SHADOW and UNINTERN.

--- Code/generic-functions.lisp
+++ Code/generic-functions.lisp
@@ -14,9 +14,10 @@
 
 ;;; This function can be used to implement the standard function
 ;;; IMPORT.  If, as a result of a call to this function, SYMBOL is
-;;; imported into PACKAGE, and SYMBOL has no home packge then client
-;;; code is responsible for setting the home package of SYMBOL to
-;;; PACKAGE.
+;;; imported into PACKAGE, and SYMBOL has no home packge (as
+;;; determined by a call to SYMBOL-PACKAGE) then this function calls
+;;; (SETF SYMBOL-PACKAGE) with PACKAGE, CLIENT, and SYMBOL as
+;;; arguments.
 (defgeneric import (client package symbol))
 
 ;;; This function can be used to implement the standard function
@@ -44,8 +45,23 @@
 
 ;;; This function can be used to implement the standard function
 ;;; SHADOW.  It differs from the standard function in that it takes a
-;;; single symbol as opposed to a list of string designators. Client
+;;; single string as opposed to a list of string designators. Client
 ;;; code for the standard function must then call this function
-;;; multiple times and use FIND-SYMBOL to determine whether an
-;;; existing symbol should be provided, or a new symbol created.
-(defgeneric shadow (client package symbol))
+;;; multiple times.  This function calls FIND-SYMBOL to determine
+;;; whether a symbol with the name NAME is present (i.e., it is
+;;; directly accessible) in PACKAGE.  If that is the case, then that
+;;; existing symbol is added to the set of shadowing symbols of
+;;; PACKAGE.  If no such symbol is present in PACKAGE, this function
+;;; calls MAKE-SYMBOL, passing it CLIENT, NAME, and PACKAGE as
+;;; arguments.  The resulting symbol is then added to PACKAGE as an
+;;; internal symbol and as a shadowing symbol.
+(defgeneric shadow (client package name))
+
+;;; This function can be used to implement the standard function
+;;; UNINTERN.  If SYMBOL is present in PACKAGE it is removed so that
+;;; it is no longer present.  It is also removed from the list of
+;;; shadowing symbols of PACKAGE.  If PACKGE is the home package of
+;;; SYMBOL, as determined by a call to SYMBOL-PACKAGE, then this
+;;; function calls (SETF SYMBOL-PACKAGE) with NIL, CLIENT, and SYMBOL
+;;; as arguments.
+(defgeneric unintern (client package symbol))
