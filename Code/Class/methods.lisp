3e03ebdec5febf3edc04d37ac9c9bc7ffd2a585c
Robert Strandh <robert.strandh@gmail.com>
2023-10-05 10:44:49 +0200

    Add the CONS cell to the hash table.

--- Code/Class/methods.lisp
+++ Code/Class/methods.lisp
@@ -42,7 +42,7 @@
 (defmethod parcl:add-external-symbol ((client client) package symbol)
   (push symbol (external-symbols-list package))
   (setf (gethash (symbol-name symbol) (external-symbols-table package))
-        (first (external-symbols-list package))))
+        (external-symbols-list package)))
 
 (defmethod parcl:internal-symbols ((client client) package)
   (internal-symbols-list package))
@@ -56,7 +56,7 @@
 (defmethod parcl:add-internal-symbol ((client client) package symbol)
   (push symbol (internal-symbols-list package))
   (setf (gethash (symbol-name symbol) (internal-symbols-table package))
-        (first (internal-symbols-list package))))
+        (internal-symbols-list package)))
 
 ;;; The technique we use for removing a symbol from the list works
 ;;; like this: The hash table contains the CONS cells of the list.
@@ -102,7 +102,7 @@
 (defmethod parcl:add-internal-symbol ((client client) package symbol)
   (push symbol (internal-symbols-list package))
   (setf (gethash (symbol-name symbol) (internal-symbols-table package))
-        (first (internal-symbols-list package))))
+        (internal-symbols-list package)))
 
 (defmethod parcl:remove-internal-symbol ((client client) package symbol)
   (let* ((table (internal-symbols-table package))
