adefb5915b11d9ea4bbdbd5260069aa25b613e84
Robert Strandh <robert.strandh@gmail.com>
2023-10-06 15:29:52 +0200

    Simplify the class representation of a package.

--- Code/Class/package-defclass.lisp
+++ Code/Class/package-defclass.lisp
@@ -20,41 +20,29 @@
 
 (defgeneric (setf used-by-list) (new-used-by-list package))
 
-;;; There can be a large number of external and internal symbols.
+;;; There can be a large number of present symbols in a package.
 ;;; However, we think that the most frequent operation is FIND-SYMBOL,
 ;;; and perhaps also INTERN.  As a result, we use a dual
-;;; representation for each of these sets of symbols, namely a hash
-;;; table to be used by FIND-SYMBOL and a simple list of all symbols
-;;; to be used by iterators and LOOP clauses.  When we need to remove
-;;; a symbol from one of the sets, we must then traverse the list, but
-;;; we think this operation is infrequent.
+;;; representation for the present symbols, namely a "symbol table"
+;;; (usually a hash table) provided by client code, and a simple list
+;;; of all symbols to be used by iterators and LOOP clauses.  When we
+;;; need to remove a symbol (by UNINTERN), we must then traverse the
+;;; list, but we think this operation is infrequent.  A symbol in the
+;;; symbol table and in the list is represented by an "entry" in the
+;;; form of a CONS cell.  The CAR of the CONS cell is the symbol, and
+;;; the CDR is the "status" of the symbol.  The status can be one of
+;;; :INTERNAL, :INTERNAL-SHADOWING, :EXTERNAL, and
+;;; :EXTERNAL-SHADOWING.
 
-;;; Return the hash table holding external symbols.  The name of the
-;;; symbol is the key.
-(defgeneric external-symbols-table (package))
+;;; Return the symbol table of PACKAGE.  The name of the symbol is the
+;;; key.
+(defgeneric symbol-table (package))
 
-;;; Return the list of external symbols.
-(defgeneric external-symbols-list (package))
+;;; Return the list of entries of every present symbol in PACKAGE.
+(defgeneric symbol-entries (package))
 
-;;; Set the list of external symbols.
-(defgeneric (setf external-symbols-list) (new-symbols-list package))
-
-;;; Return the hash table holding internal symbols.  The name of the
-;;; symbol is the key.
-(defgeneric internal-symbols-table (package))
-
-;;; Return the list of internal symbols.
-(defgeneric internal-symbols-list (package))
-
-;;; Set the list of internal symbols.
-(defgeneric (setf internal-symbols-list) (new-symbols-list package))
-
-;;; We represent the shadowing symbols as a simple list, assuming that
-;;; in general there won't be very many of them.
-
-(defgeneric shadowing-symbols (package))
-
-(defgeneric (setf shadowing-symbols) (new-shadowing-symbols package))
+;;; Set the list of entries of every present symbol in PACKAGE.
+(defgeneric (setf symbol-entries) (new-entries package))
 
 (defclass package (parcl:package)
   ((%name
@@ -76,19 +64,9 @@
       :initarg :used-by-list
       :initform '()
       :accessor used-by-list)
-   (%external-symbols-table
-      :initarg :external-symbols
-      :reader external-symbols-table)
-   (%external-symbols-list
+   (%symbol-table
+      :initarg :symbol-table
+      :reader symbol-table)
+   (%symbol-entries
       :initform '()
-      :accessor external-symbols-list)
-   (%internal-symbols-table
-      :initarg :internal-symbols
-      :reader internal-symbols-table)
-   (%internal-symbols-list
-      :initform '()
-      :accessor internal-symbols-list)
-   (%shadowing-symbols
-      :initarg :shadowing-symbols
-      :initform '()
-      :accessor shadowing-symbols)))
+      :accessor symbol-entries)))
