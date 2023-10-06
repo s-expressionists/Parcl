4b955053897baf40c173a041518961ad6a14631c
Robert Strandh <robert.strandh@gmail.com>
2023-10-06 17:09:53 +0200

    Fix incorrect actions when there is a conflict.

--- Code/import-defmethod.lisp
+++ Code/import-defmethod.lisp
@@ -11,8 +11,7 @@
               (error 'symbol-conflict
                      :conflicting-symbols (list symbol external-symbol))
             (unintern-existing-symbol ()
-              (remove-external-symbol client package external-symbol)
-              (add-internal-symbol client package symbol))
+              (unintern client package symbol))
             (do-not-import ())))
         (return-from import t)))
     (multiple-value-bind (internal-symbol present-p)
@@ -24,8 +23,7 @@
               (error 'symbol-conflict
                      :conflicting-symbols (list symbol internal-symbol))
             (unintern-existing-symbol ()
-              (remove-internal-symbol client package internal-symbol)
-              (add-internal-symbol client package symbol))
+              (unintern client package symbol))
             (do-not-import ())))
         (return-from import t)))
     (multiple-value-bind (shadowing-symbol present-p)
@@ -37,22 +35,21 @@
               (error 'symbol-conflict
                      :conflicting-symbols (list symbol shadowing-symbol))
             (unintern-existing-symbol ()
-              (remove-shadowing-symbol client package shadowing-symbol)
-              (add-internal-symbol client package symbol))
+              (unintern client package symbol))
             (do-not-import ())))
         (return-from import t)))
     (loop for used-package in (use-list client package)
-          do (multiple-value-bind (inherited-symbol present-p)
-                 (find-external-symbol client used-package name)
-               (when present-p
+          do (multiple-value-bind (inherited-symbol status)
+                 (find-present-symbol client used-package name)
+               (when (eq status :external)
                  (if (eq symbol inherited-symbol)
-                     (add-internal-symbol client package symbol)
+                     (ensure-present-symbol client package symbol)
                      ;; We have a conflict.
                      (restart-case
                          (error 'symbol-conflict
                                 :conflicting-symbols
                                 (list symbol inherited-symbol))
                        (make-a-shadowing-symbol ()
-                         (add-shadowing-symbol client package symbol))
+                         (shadowing-import client package symbol))
                        (do-not-import ())))
                  (return-from import t))))))
