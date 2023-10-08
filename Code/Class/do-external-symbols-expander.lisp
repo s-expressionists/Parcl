2b2902cbd0b2aec54fbb654e640b3e26b7233796
Robert Strandh <robert.strandh@gmail.com>
2023-10-08 14:53:46 +0200

    Transform expander functions into mapping functions.

--- Code/Class/do-external-symbols-expander.lisp
+++ Code/Class/do-external-symbols-expander.lisp
@@ -1,20 +1,7 @@
 (cl:in-package #:parcl-class)
 
-(defmethod parcl:do-external-symbols-expander
-    ((client client)
-     symbol-varaible
-     package-designator-form
-     result-form
-     body)
-  (let ((package-variable (gensym)))
-    (multiple-value-bind (declarations tags-and-statements)
-        (separate-ordinary-body body)
-      `(flet ((body-function (,symbol-variable)
-                ,@declarations
-                (tagbody ,tags-and-statements)))
-         (let* ((,package-variable (find-package ,package)))
-           (loop for entry in (symbol-entries ,package-variable)
-                 when (or (eq (entry-status entry) :external)
-                          (eq (entry-status entry) :external-shadowing))
-                   do (body-function symbol)))
-         ,result-form))))
+(defmethod parcl:map-external-symbols (client package function)
+  (loop for entry in (symbol-entries package)
+        when (or (eq (entry-status entry) :external)
+                 (eq (entry-status entry) :external-shadowing))
+          do (funcall function symbol)))
--- Code/Class/do-symbols-expander.lisp
+++ Code/Class/do-symbols-expander.lisp
@@ -1,29 +1,16 @@
 (cl:in-package #:parcl-class)
 
-(defmethod parcl:do-symbols-expander
-    ((client client)
-     symbol-varaible
-     package-designator-form
-     result-form
-     body)
-  (let ((package-variable (gensym)))
-    (multiple-value-bind (declarations tags-and-statements)
-        (separate-ordinary-body body)
-      `(flet ((body-function (,symbol-variable)
-                ,@declarations
-                (tagbody ,tags-and-statements)))
-         (let* ((,package-variable (find-package ,package))
-                (visited 
-                  (loop for entry in (symbol-entries ,package-variable)
-                        for symbol = (entry-symbol entry)
-                        do (body-function symbol)
-                        collect symbol)))
-           (loop for package in (use-list ,package-variable)
-                 do (do-external-symbols (symbol package)
-                      (unless (member symbol visited
-                                      :test (lambda (symbol1 symbol2)
-                                              (parcl:symbol-names-equal
-                                               *client* symbol1 symbol2)))
-                        (body-function symbol)
-                        (push symbol visited)))))
-         ,result-form))))
+(defmethod parcl:map-symbols (client package function)
+  (let ((visited 
+          (loop for entry in (symbol-entries package)
+                for symbol = (entry-symbol entry)
+                do (body-function symbol)
+                collect symbol)))
+    (loop for package in (use-list package)
+          do (do-external-symbols (symbol package)
+               (unless (member symbol visited
+                               :test (lambda (symbol1 symbol2)
+                                       (parcl:symbol-names-equal
+                                        *client* symbol1 symbol2)))
+                 (funcall function symbol)
+                 (push symbol visited))))))
