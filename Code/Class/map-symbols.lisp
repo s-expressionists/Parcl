f29e3904c9949959c09bb10d18c895640d5a23ab
Robert Strandh <robert.strandh@gmail.com>
2023-10-08 15:30:52 +0200

    Use MAP-EXTERNAL-SYMBOL rather than DO-EXTERNAL-SYMBOLS.

--- Code/Class/map-symbols.lisp
+++ Code/Class/map-symbols.lisp
@@ -4,13 +4,15 @@
   (let ((visited 
           (loop for entry in (symbol-entries package)
                 for symbol = (entry-symbol entry)
-                do (body-function symbol)
+                do (funcall function symbol)
                 collect symbol)))
     (loop for package in (use-list package)
-          do (do-external-symbols (symbol package)
-               (unless (member symbol visited
-                               :test (lambda (symbol1 symbol2)
-                                       (parcl:symbol-names-equal
-                                        *client* symbol1 symbol2)))
-                 (funcall function symbol)
-                 (push symbol visited))))))
+          do (parcl:map-external-symbols
+              client package
+              (lambda (symbol)
+                (unless (member symbol visited
+                                :test (lambda (symbol1 symbol2)
+                                        (parcl:symbol-names-equal
+                                         client symbol1 symbol2)))
+                  (funcall function symbol)
+                  (push symbol visited)))))))
