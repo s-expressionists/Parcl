713414aacc29c0bb6cc12ae03385f1b439b33c17
Robert Strandh <robert.strandh@gmail.com>
2023-10-09 06:25:09 +0200

    Fix several problems in IMPORT.

--- Code/import-defmethod.lisp
+++ Code/import-defmethod.lisp
@@ -5,16 +5,17 @@
   (let ((name (symbol-name client symbol)))
     (multiple-value-bind (present-symbol status)
         (find-present-symbol client package name)
-      (unless (null status)
-        (unless (eq symbol present-symbol)
-          ;; We have a conflict.
-          (restart-case
-              (error 'symbol-conflict
-                     :conflicting-symbols (list symbol present-symbol))
-            (unintern-existing-symbol ()
-              (unintern client package symbol))
-            (do-not-import ())))
-        (return-from import t)))
+      (unless (or (null status) (eq symbol present-symbol))
+        ;; We have a conflict.
+        (restart-case
+            (error 'symbol-conflict
+                   :conflicting-symbols (list symbol present-symbol))
+          (unintern-existing-symbol ()
+            (unintern client package present-symbol)
+            (ensure-present-symbol client package symbol :internal)
+            (return-from import t))
+          (do-not-import ()
+            (return-from import t)))))
     (loop for used-package in (use-list client package)
           do (multiple-value-bind (inherited-symbol status)
                  (find-present-symbol client used-package name)
@@ -27,6 +28,9 @@
                                 :conflicting-symbols
                                 (list symbol inherited-symbol))
                        (make-a-shadowing-symbol ()
-                         (shadowing-import client package symbol))
-                       (do-not-import ())))
-                 (return-from import t))))))
+                         (shadowing-import client package symbol)
+                         (return-from import t))
+                       (do-not-import ()
+                           (return-from import t)))))))
+    (ensure-present-symbol client package symbol :internal)
+    (return-from import t)))
