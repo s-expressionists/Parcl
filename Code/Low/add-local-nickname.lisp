615cdc1026a72b050fdee9e66d7c27d9546e23d4
Robert Strandh <robert.strandh@gmail.com>
2024-05-01 09:17:26 +0200

    Define method on ADD-LOCAL-NICKNAME.

--- Code/Low/add-local-nickname.lisp
+++ Code/Low/add-local-nickname.lisp
@@ -1,1 +1,19 @@
 (cl:in-package #:parcl-low)
+
+(defmethod add-local-nickname (client nickname nicknamed-package package)
+  (let* ((nickname-string (string nickname))
+         (existing-nickname-pair
+           (assoc nickname-string
+                  (local-nicknames client package)
+                  :test #'string=)))
+    (if (and (not (null existing-nickname-pair))
+             (not (eq nicknamed-package (second existing-nickname-pair))))
+        ;; FIXME: signal a continuable error.
+        (error 'nickname-refers-to-different-package
+               :nickname nickname-string
+               :nicknamed-package nicknamed-package
+               :package package)
+        (progn (push (list nickname-string nicknamed-package)
+                     (local-nicknames client package))
+               (push package
+                     (locally-nicknamed-by client nicknamed-package))))))
