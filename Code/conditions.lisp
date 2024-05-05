6a3aa3d4e09dc68fefa21effa67806410d265ed2
Robert Strandh <robert.strandh@gmail.com>
2024-05-05 16:46:43 +0200

    Delete definitions of condition types now in Low system.

--- Code/conditions.lisp
+++ Code/conditions.lisp
@@ -32,27 +32,3 @@
 
 (define-condition package-error (error)
   ((%package :initarg :package :reader package-error-package)))
-
-(define-condition symbol-conflict (package-error)
-  ((%conflicting-symbols
-      :initarg :conflicting-symbols
-      :reader conflicting-symbols)))
-
-(define-condition symbol-is-not-accessible (package-error)
-  ((%symbol :initarg :symbol :reader inaccessible-symbol)))
-
-;;; This condition is signaled by UNUSE-PACKAGE when the package is
-;;; not used, so that it can't be unused.
-(define-condition package-is-not-used (package-error)
-  ((%package-to-unuse
-    :initarg :package-to-unuse
-    :reader package-to-unuse))
-  (:report (lambda (condition stream)
-             (format stream
-                     "A package to be unused must be a used package,~@
-                      but the package:~@
-                      ~s~@
-                      is not used by the package:~@
-                      ~s"
-                     (package-to-unuse condition)
-                     (package-error-package condition)))))
