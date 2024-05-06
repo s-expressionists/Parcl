b0a9ab55f2949b2edb5c0401fbe3072b576f76c3
Robert Strandh <robert.strandh@gmail.com>
2024-05-06 15:25:17 +0200

    New condition type SYMBOLS-MUST-BE-DESIGNATOR-FOR-LIST-OF-SYMBOLS.

--- Code/condition-types.lisp
+++ Code/condition-types.lisp
@@ -9,3 +9,12 @@
                       ~s"
                      (type-error-datum condition))))
   (:default-initargs :expected-type 'string))
+
+(define-condition symbols-must-be-designator-for-list-of-symbols (error)
+  ((%symbols :initarg :symbols :reader symbols))
+  (:report (lambda (condition stream)
+             (format stream
+                     "Argument must be a designator for a list of symbols,~@
+                      but the following was found instead:~@
+                      ~s"
+                     (symbols condition)))))
