1c262e8fa6d31a1f4a8ffaab756b2132154c09d7
Robert Strandh <robert.strandh@gmail.com>
2024-05-07 16:28:52 +0200

    Add nickname to extrinsic and intrinsic packages.

--- Code/packages-extrinsic.lisp
+++ Code/packages-extrinsic.lisp
@@ -1,6 +1,7 @@
 (cl:in-package #:common-lisp-user)
 
 (defpackage parcl
+  (:nicknames #:parcl-extrinsic)
   (:use #:common-lisp)
   (:shadow . #.parcl-asdf:*string-designators*)
   (:export . #.(append parcl-asdf:*string-designators*
--- Code/packages-intrinsic.lisp
+++ Code/packages-intrinsic.lisp
@@ -1,6 +1,7 @@
 (cl:in-package #:common-lisp-user)
 
 (defpackage parcl
+  (:nicknames #:parcl-intrinsic)
   (:use #:common-lisp)
   (:export . #.(append parcl-asdf:*string-designators*
                        parcl-asdf:*exports*)))
