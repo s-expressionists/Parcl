e8c69b5447e9d6006f1b5b325ea601fe3cad900b
Robert Strandh <robert.strandh@gmail.com>
2024-05-09 11:23:33 +0200

    Remove package nicknames.

--- Code/packages-extrinsic.lisp
+++ Code/packages-extrinsic.lisp
@@ -1,7 +1,6 @@
 (cl:in-package #:common-lisp-user)
 
 (defpackage parcl
-  (:nicknames #:parcl-extrinsic)
   (:use #:common-lisp)
   (:shadow . #.parcl-asdf:*string-designators*)
   (:export . #.(append parcl-asdf:*string-designators*
--- Code/packages-intrinsic.lisp
+++ Code/packages-intrinsic.lisp
@@ -1,7 +1,6 @@
 (cl:in-package #:common-lisp-user)
 
 (defpackage parcl
-  (:nicknames #:parcl-intrinsic)
   (:use #:common-lisp)
   (:export . #.(append parcl-asdf:*string-designators*
                        parcl-asdf:*exports*)))
