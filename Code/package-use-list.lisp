677182d6b884019904957e746a616fb5d78f77d1
Robert Strandh <robert.strandh@gmail.com>
2024-05-12 16:14:13 +0200

    Provide documentation string for PACKAGE-USE-LIST.

--- Code/package-use-list.lisp
+++ Code/package-use-list.lisp
@@ -2,3 +2,19 @@
 
 (defun package-use-list (package-designator)
   (parcl-low:use-list *client* (find-package package-designator)))
+
+(setf (documentation 'package-use-list 'function)
+      (format nil
+              "Syntax: package-use-list package-designator~@
+               ~@
+               This function returns the use list of the package~@
+               designated by PACKAGE-DESIGNATOR as a list of package~@
+               objects.~@
+               ~@
+               If PACKAGE-DESIGNATOR is not a package designator,~@
+               then an error of type TYPE-ERROR is signaled.  If~@
+               PACKAGE-DESIGNATOR is a string designator and no package~@
+               with the designated name exists, then an error of type~@
+               PACKAGE-ERROR is signaled."))
+
+; LocalWords:  designator
