824bdaec2edfef71af5db48354967d29a029e372
Robert Strandh <robert.strandh@gmail.com>
2024-05-12 15:46:02 +0200

    Provide documentation string for MAKE-PACKAGE.

--- Code/make-package.lisp
+++ Code/make-package.lisp
@@ -13,3 +13,28 @@
       (parcl-low:use-packages *client* result canonicalized-packages)
       (store-package result canonicalized-name canonicalized-nicknames)
       result)))
+
+(setf (documentation 'make-package 'function)
+      (format nil
+              "Syntax: make-package name &key nicknames use~@
+               ~@
+               This function creates a new package and makes it~@
+               available in the global environment so that FIND-PACKAGE~@
+               can find and return it.~@
+               ~@
+               NAME is a string designator that determines the name~@
+               of the new package.  NICKNAMES is a list of string~@
+               designators containing the nicknames to be given to the~@
+               new package.  If NICKNAMES is not supplied, it defaults~@
+               to the empty list.  USE is a list of package designators~@
+               where the corresponding packages will determine the initial~@
+               use list of the new package.  If USE is not given, it~@
+               defaults to the empty list.~@
+               ~@
+               If any of the packages designated by USE does not exist,~@
+               then an error of type PACKAGE-ERROR is signaled.  If NAME~@
+               or any of the names in NICKNAMES is the name of an existing~@
+               package, then a correctable error of type PACKAGE-ERROR is~@
+               signaled."))
+
+; LocalWords:  designator designators
