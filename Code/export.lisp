4d6017a57a0568db6640a93a9914323dcce8fb87
Robert Strandh <robert.strandh@gmail.com>
2024-05-14 13:47:13 +0200

    Provide documentation string for EXPORT.

--- Code/export.lisp
+++ Code/export.lisp
@@ -10,3 +10,35 @@
         (package (find-package package-designator)))
     (loop for symbol in symbols
           do (parcl-low:export *client* package symbol))))
+
+(setf (documentation 'export 'function)
+      (format nil
+              "Syntax: export symbols &optional package-designator~@
+              ~@
+               This function causes the symbols designated by SYMBOLS to~@
+               become external symbols of the package designated by~@
+               PACKAGE-DESIGNATOR.  If PACKAGE-DESIGNATOR is not supplied,~@
+               then it defaults to the current package, i.e., the value~@
+               of *PACKAGE*.~@
+               ~@
+               SYMBOLS is a designator for a list of symbols.~@
+               In other words, if SYMBOLS is a symbol other than NIL,~@
+               then that single symbol is designated.  If SYMBOLS is~@
+               NIL, then no symbols are designated.  Otherwise, SYMBOLS~@
+               must be a proper list of symbols in which case the elements~@
+               of that list are the designated symbols.~@
+               ~@
+               If SYMBOLS is not a designator for a list of symbols,~@
+               then an error is signaled.  If PACKAGE-DESIGNATOR is given,~@
+               but it is not a package designator, then an error of type~@
+               TYPE-ERROR is signaled.  If PACKAGE-DESIGNATOR is a string~@
+               designator, and no package with the designated name exists,~@
+               then an error of type PACKAGE-ERROR is signaled.~@
+               ~@
+               If any of the symbols designated by SYMBOLS is not~@
+               accessible in the package designated by PACKAGE-DESIGNATOR,~@
+               a correctable error of type PACKAGE-ERROR is signaled,~@
+               allowing the user to import the symbol first, and thereby~@
+               making it accessible"))
+
+; LocalWords:  designator
