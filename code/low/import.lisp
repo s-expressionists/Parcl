9a50b8a3d025673288de6efbbef479832dd9e989
Jan Moringen <jan.moringen@posteo.de>
2025-06-27 17:30:18 +0200

    UNFINISHED Improve PARCL-LOW:IMPORT

--- code/low/import.lisp
+++ code/low/import.lisp
@@ -2,6 +2,7 @@
 
 (defmethod import (client package symbol)
   (let ((name (symbol-name client symbol)))
+    ;; Check for name conflict with present symbols.
     (multiple-value-bind (present-symbol status)
         (find-present-symbol client package name)
       (unless (or (null status) (eq symbol present-symbol))
@@ -11,9 +12,11 @@
                    :conflicting-symbols (list symbol present-symbol))
           (unintern-existing-symbol ()
             (unintern client package present-symbol)
-            (ensure-present-symbol client package symbol :internal))
+            ;; TODO(jmoringe): should we skip the other conflict check?
+            )
           (do-not-import ()
             (return-from import t)))))
+    ;; Check for name conflict with inherited symbols.
     (loop for used-package in (use-list client package)
           do (multiple-value-bind (inherited-symbol status)
                  (find-present-symbol client used-package name)
@@ -29,6 +32,8 @@
                          (shadowing-import client package symbol)
                          (return-from import t))
                        (do-not-import ()
-                           (return-from import t)))))))
+                         (return-from import t)))))))
+    ;;
     (ensure-present-symbol client package symbol :internal)
-    (return-from import t)))
+    (setf (symbol-package client symbol) package)
+    t))
