79bee356caf249ab3a7b76a50737da582cb89d6d
Jan Moringen <jan.moringen@posteo.de>
2025-06-27 16:39:09 +0200

    Restructure and comment the definitions of client-facing packages a bit

--- Code/packages-extrinsic.lisp
+++ Code/packages-extrinsic.lisp
@@ -1,16 +1,21 @@
 (cl:defpackage #:parcl
   (:use
    #:common-lisp)
+  ;; Shadow Common Lisp symbols in order to provide replacements of
+  ;; the same (symbol-)name for the designated definitions.
   (:shadow
-   . #1=(#:*package*
+   . #1=(;; Variables
+         #:*package*
+         ;; Conditions
+         #:package-error
+         #:package-error-package
+         ;; Functions
          #:find-package
          #:package-name
          #:package-nicknames
          #:package-shadowing-symbols
          #:package-use-list
          #:package-used-by-list
-         #:package-error
-         #:package-error-package
          #:rename-package
          #:make-package
          #:import
@@ -23,13 +28,21 @@
          #:shadowing-import
          #:unuse-package
          #:use-package
+         ;; Macros
          #:defpackage
          #:with-package-iterator
          #:do-symbols
          #:do-external-symbols))
+  ;; Export names of the replacements for the shadowed Common Lisp
+  ;; variables, condition types and operators.
+  (:export . #1#)
+  ;; Export names of operators for the package-local nicknames
+  ;; extension.
+  (:export
+   #:add-package-local-nickname
+   #:remove-package-local-nickname)
+  ;; Export the names of additional variables and functions that
+  ;; clients can use to customize the package system.
   (:export
    #:*client*
-   #:add-package-local-nickname
-   #:remove-package-local-nickname
-   #:store-package
-   .  #1#))
+   #:store-package))
--- Code/packages-intrinsic.lisp
+++ Code/packages-intrinsic.lisp
@@ -1,8 +1,13 @@
 (cl:defpackage #:parcl
   (:use
    #:common-lisp)
+  ;; Export names of operators for the package-local nicknames
+  ;; extension.
+  (:export
+   #:add-package-local-nickname
+   #:remove-package-local-nickname)
+  ;; Export the names of additional variables and functions that
+  ;; clients can use to customize the package system.
   (:export
    #:*client*
-   #:add-package-local-nickname
-   #:remove-package-local-nickname
    #:store-package))
