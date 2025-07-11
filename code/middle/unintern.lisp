8481ce8191ec42115b05bdcb19e71861ef1c0510
Jan Moringen <jan.moringen@posteo.de>
2025-07-11 02:53:22 +0200

    middle: Improve UNINTERN

--- code/middle/unintern.lisp
+++ code/middle/unintern.lisp
@@ -9,22 +9,28 @@
                  (pushnew symbol result :test #'eq))))
     result))
 
-(defmethod unintern (client package symbol)
+(defmethod unintern ((client t) (package t) (symbol t))
   (let ((name (low:symbol-name client symbol)))
     ;; TODO: use (map-accessible-entries-with-name)
     (multiple-value-bind (present-symbol status)
         (low:symbol-entry client name package)
-      (cond ((or (null status) (not (eq present-symbol symbol)))
-             nil)
-            ((member symbol (shadowing-symbols client package)) ; TODO: can't we tell from STATUS?
-             (let* ((used-packages (low:use-list client package))
-                    (symbols (find-exported-symbols-in-packages
-                              client used-packages name)))
-               (if (> (length symbols) 1)
+      (flet ((remove-symbol ()
+               (setf (low:symbol-entry client name package) nil)
+               ;; If PACKAGE is the home package of SYMBOL, reset the
+               ;; home package to `nil'.
+               (when (eq (low:symbol-package client symbol) package)
+                 (setf (low:symbol-package client symbol) nil))))
+        (cond ((or (null status) (not (eq present-symbol symbol)))
+               nil)
+              ((member symbol (shadowing-symbols client package)) ; TODO: can't we tell from STATUS?
+               (let* ((used-packages (low:use-list client package))
+                      (symbols       (find-exported-symbols-in-packages
+                                      client used-packages name)))
+                 (when (> (length symbols) 1)
                    ;; We have a conflict.  For now just signal an error.
-                   (error "Symbol conflict, not uninterning ~s" symbol)
-                   (progn
-                     (setf (low:symbol-entry client name package) nil)
-                     t))))
-            (t
-             (setf (low:symbol-entry client name package) nil))))))
+                   (error "Symbol conflict, not uninterning ~s" symbol)))
+               (remove-symbol)
+               t)
+              (t
+               (remove-symbol)
+               t))))))
