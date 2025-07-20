831b1cd56302036016c1acb8b50957321ad26881
Jan Moringen <jan.moringen@posteo.de>
2025-07-21 01:40:31 +0200

    UNFINISHED | macros: Update DO[-EXTERNAL]-SYMBOLS

--- code/macros/do-external-symbols.lisp
+++ code/macros/do-external-symbols.lisp
@@ -1,17 +1,26 @@
 (cl:in-package #:parcl)
 
-(defmacro do-external-symbols
-    ((symbol-variable
-      &optional
-        (package-designator-form '*package*)
-        (result-form 'nil))
-     &body body)
-  (let ((package-variable (gensym)))
-    (multiple-value-bind (declarations tags-and-statements)
-        (ecclesia:separate-ordinary-body body)
-      `(flet ((body-function (,symbol-variable)
+(defun %map-external-symbols (function package-designator)
+  (let ((package (find-package package-designator)))
+    (flet ((one-symbol (symbol export-status shadow-status)
+             (declare (ignore shadow-status))
+             (when (eq export-status :external)
+               (funcall function symbol))))
+      ;; TODO: pass desired status
+      (parcl-low:map-symbol-entries *client* #'one-symbol package))))
+
+(defmacro do-external-symbols ((symbol-variable
+                                &optional (package-designator-form '*package*)
+                                          (result-form 'nil))
+                               &body body)
+  (multiple-value-bind (declarations tags-and-statements)
+      (ecclesia:separate-ordinary-body body)
+    `(block nil
+       (flet ((body-function (,symbol-variable &optional donep)
                 ,@declarations
-                (tagbody ,tags-and-statements)))
-         (let ((,package-variable (find-package ,package-designator-form)))
-           (map-external-symbols *client* ,package-variable #'body-function))
-         ,result-form))))
+                (declare (ignorable ,symbol-variable))
+                (if (not donep)
+                    (tagbody ,@tags-and-statements)
+                    (return ,result-form))))
+         (%map-external-symbols #'body-function ,package-designator-form)
+         (body-function nil t)))))
--- code/macros/do-symbols.lisp
+++ code/macros/do-symbols.lisp
@@ -1,17 +1,24 @@
 (cl:in-package #:parcl)
 
-(defmacro do-symbols
-    ((symbol-variable
-      &optional
-        (package-designator-form '*package*)
-        (result-form 'nil))
-     &body body)
-  (let ((package-variable (gensym)))
-    (multiple-value-bind (declarations tags-and-statements)
-        (ecclesia:separate-ordinary-body body)
-      `(flet ((body-function (,symbol-variable)
+(defun %map-symbols (function package-designator)
+  (let ((package (find-package package-designator)))
+    (flet ((one-symbol (containing-package symbol export-status shadow-status)
+             (declare (ignore containing-package export-status shadow-status))
+             (funcall function symbol)))
+      (parcl.middle::map-accessible-entries *client* #'one-symbol package))))
+
+(defmacro do-symbols ((symbol-variable
+                       &optional (package-designator-form '*package*)
+                                 (result-form 'nil))
+                      &body body)
+  (multiple-value-bind (declarations tags-and-statements)
+      (ecclesia:separate-ordinary-body body)
+    `(block nil
+       (flet ((body-function (,symbol-variable &optional donep)
                 ,@declarations
-                (tagbody ,@tags-and-statements)))
-         (let ((,package-variable (find-package ,package-designator-form)))
-           (parcl-low:map-symbols *client* ,package-variable #'body-function))
-         ,result-form))))
+                (declare (ignorable ,symbol-variable))
+                (if (not donep)
+                    (tagbody ,@tags-and-statements)
+                    (return ,result-form))))
+         (%map-symbols #'body-function ,package-designator-form)
+         (body-function nil t)))))
