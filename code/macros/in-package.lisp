905c04c0ef13eeff0f631ff728adf64252f10319
Jan Moringen <jan.moringen@posteo.de>
2025-10-10 00:25:14 +0200

    macros: Split IN-PACKAGE into the macro and EXPAND-IN-PACKAGE

--- code/macros/in-package.lisp
+++ code/macros/in-package.lisp
@@ -1,9 +1,10 @@
 (cl:in-package #:parcl)
 
-(defmacro in-package (&whole form package-designator)
-  (declare (ignore package-designator))
-  (let* ((ast          (parse form :replace-operator 'cl:in-package))
-         (package-name (ico:designated-string (ico:name-ast ast))))
-    `(eval-when (:compile-toplevel :load-toplevel :execute)
-       ;; TODO: should this take into account local nicknames or generally `find-package-using-package'?
-       (setf *package* (find-undeleted-package-or-error *client* ,package-name)))))
+(defun expand-in-package (package-name)
+  `(eval-when (:compile-toplevel :load-toplevel :execute)
+     ;; TODO: should this take into account local nicknames or generally `find-package-using-package'?
+     (setf *package* (find-undeleted-package-or-error *client* ,package-name))))
+
+(define-macro in-package (package-designator) ast
+  (let ((package-name (ico:designated-string (ico:name-ast ast))))
+    (expand-in-package package-name)))
