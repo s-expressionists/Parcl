e40136dede3244e54a08710889e1889eb74bb5f5
Jan Moringen <jan.moringen@posteo.de>
2025-10-10 00:24:34 +0200

    macros: Add s-expression-syntax-based macro utilities

--- code/macros/utilities.lisp
+++ code/macros/utilities.lisp
@@ -1,13 +1,29 @@
 (cl:in-package #:parcl)
 
-(defun parse (form &key replace-operator)
-  (let* ((builder       (make-instance 'iconoclast-builder:builder))
-         (modified-form (list* replace-operator (rest form)))) ; TODO: can we avoid this?
-    (handler-case
-        (s-expression-syntax:parse builder t modified-form)
-      (s-expression-syntax:s-expression-syntax-error (condition)
-        ;; TODO: with-current-source-form ()
-        (let ((message (s-expression-syntax:message condition)))
-          (error 'macro-syntax-error
-                 :format-control   "~S"
-                 :format-arguments (list message)))))))
+(#+sbcl sb-ext:defglobal #-sbcl defvar **builder**
+  (make-instance 'iconoclast-builder:builder))
+
+(defmacro parse (syntax form)
+  `(let* ((builder       **builder**)
+          (syntax        (load-time-value
+                           (s-expression-syntax:find-syntax ',syntax)))
+          (modified-form (list* ',syntax (rest ,form)))) ; TODO: can we avoid this?
+     (handler-case
+         (s-expression-syntax:parse builder syntax modified-form)
+       (s-expression-syntax:s-expression-syntax-error (condition)
+         ;; TODO: with-current-source-form ()
+         (let ((message (progn
+                          #+TODO (s-expression-syntax:message condition)
+                          (princ-to-string condition))))
+           (error 'macro-syntax-error :format-control   "~S"
+                                      :format-arguments (list message)))))))
+
+(defmacro define-macro (name (&rest lambda-list) ast-var &body body)
+  (let ((cl-name (cl:intern (string name) '#:common-lisp)))
+    `(progn
+       (defmacro ,name (&whole form &rest rest)
+         (declare (ignore rest))
+         (let ((,ast-var (parse ,cl-name form)))
+           ,@body))
+       #+sbcl (setf (sb-c::%fun-lambda-list (macro-function ',name))
+                    ',lambda-list))))
