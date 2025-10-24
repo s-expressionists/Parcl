8008124c37f637acdfc778ef58eeb1c18e2a53f2
Jan Moringen <jan.moringen@posteo.de>
2025-10-24 11:01:56 +0200

    Add documentation strings to condition types

--- code/condition-types.lisp
+++ code/condition-types.lisp
@@ -27,30 +27,51 @@
 
 (define-condition package-name-occupied-error (error
                                                package-name-occupied-condition)
-  ())
+  ()
+  (:documentation
+   "This error is signaled when an attempt is made to associate a,
+possibly newly created, package with a name or nickname that is
+already a name or nickname of a different package."))
 
 (define-condition package-error (error package-system-condition)
   ((%package :initarg #1=:package
              :reader  package-error-package))
   (:default-initargs
-   #1# (error "Required argument ~s" #1#)))
+   #1# (error "~@<The required initarg ~S has not been supplied.~@:>" #1#))
+  (:documentation
+   "This error is the super-type for errors that refer a particular package.
+
+Direct instances of this condition type should not be signaled."))
 
 (define-condition new-name-occupied-error (package-error
                                            package-name-occupied-condition)
-  ())
+  ()
+  (:documentation
+   "This error is signaled when an attempt is made to associate an already
+existing package with a new name or new nickname that is already a
+name or nickname of a different package."))
 
 (define-condition package-does-not-exist-error (package-error)
-  ())
+  ()
+  (:documentation
+   "This error is signaled when a name that is not the name or nickname of
+a package is supplied as a package designator."))
 
 (define-condition package-has-been-deleted-error (package-error)
-  ())
+  ()
+  (:documentation
+   "This error is signaled when a deleted package object is supplied to an
+operator that requires an undeleted package object."))
 
 ;;; Conditions related to package-package relations
 
-;; signaled from `delete-package'
+;;; Signaled from `delete-package'
 (define-condition package-in-use-error (package-error)
   ((%used-by :initarg :used-by
-             :reader  used-by)))
+             :reader  used-by))
+  (:documentation
+   "This error is signaled when an attempt is made to delete a package
+that is in use by a different package."))
 
 ;;; This condition is signaled by UNUSE-PACKAGE when the package is
 ;;; not used, so that it can't be unused.
@@ -63,7 +84,11 @@
   ((%nickname          :initarg :nickname
                        :reader  nickname)
    (%nicknamed-package :initarg :nicknamed-package
-                       :reader  nicknamed-package)))
+                       :reader  nicknamed-package))
+  (:documentation
+   "This error is signaled when an attempt is made to install a local
+nickname in a package in which that local nickname already refers to a
+different package."))
 
 ;;; Conditions related to package-symbol relations
 
