0599a06e57a03e9aa8d27ab662d4b8dcd9227676
Jan Moringen <jan.moringen@posteo.de>
2025-10-26 12:04:53 +0100

    macros: DEFPACKAGE expands to MIDDLE:ENSURE-PACKAGE call

--- code/macros/defpackage.lisp
+++ code/macros/defpackage.lisp
@@ -1,47 +1,8 @@
 (cl:in-package #:parcl)
 
-(sb-ext:add-package-local-nickname "ICO" "ICONOCLAST" "PARCL")
-
-;;; Runtime support
-
-(defun find-symbols (package-name &rest symbol-names)
-  (loop with client = *client* ; TODO: pass in?
-        with package = (find-undeleted-package-or-error client package-name) ; TODO(jmoringe): error
-        for symbol-name in symbol-names
-        for (symbol status) = (multiple-value-list
-                               (find-symbol symbol-name package))
-        if (not (null status))
-          collect symbol
-        else
-          do (error "No symbol named ~S in package ~A" symbol-name package) ; TODO(jmoringe): proper error
-        ))
-
-;;; Used by (:export ...).  Ensures that SYMBOL-NAMES are present, via
-;;; `intern' if not accessible and via `import' if inherited.
-(defun ensure-symbols (package &rest symbol-names)
-  (loop for symbol-name in symbol-names
-        for (symbol status) = (multiple-value-list
-                               (find-symbol symbol-name package))
-        collect (case status
-                  ((nil)
-                   (intern symbol-name package))
-                  (:inherited
-                   (import symbol package)
-                   symbol)
-                  (t
-                   symbol))))
-
-(defun intern* (package &rest symbol-names)
-  (loop with client = *client*
-        for symbol-name in symbol-names
-        do (parcl.middle:intern client package symbol-name)))
-
-;; runtime effect of defpackage; defined as a function to minimize code size of defpackage expansion
-#++ (defun define-package )
-
 ;;; Macro expander
 
-(defun check-options-disjoint (name1 value1 name2 value2) ; TODO: pass source?
+(defun check-options-disjoint (name1 value1 name2 value2) ; TODO: pass source and do with-current-source-form?
   (let ((intersection (intersection value1 value2 :test #'string=)))
     (when (not (null intersection))
       (let ((common (remove-duplicates intersection :test #'string=)))
@@ -53,14 +14,19 @@
         do (loop for (name2 . value2) in rest
                  do (check-options-disjoint name1 value1 name2 value2))))
 
-(defun expand-defpackage (name &key (nicknames             '())
-                                    (shadow                '())
-                                    (shadowing-import-from '())
-                                    (use                   '())
-                                    (import-from           '())
-                                    (intern                '())
-                                    (export                '())
-                                    (documentation         nil))
+;;; TODO: should we make this a generic function with a client parameter?
+;;; TODO: share string designators, for symbols and packages, to (potentially) safe fasl space?
+(defun expand-defpackage (name &rest args &key (nicknames             '()) ; TODO: find a consistent order for these options
+                                               (shadow                '())
+                                               (shadowing-import-from '())
+                                               (use                   '())
+                                               (import-from           '())
+                                               (intern                '())
+                                               (export                '())
+                                               (size                  nil)
+                                               (documentation         nil))
+  ;; TODO: could just not list those and &allow-other-keys
+  (declare (ignore nicknames use size documentation))
   ;; From the `defpackage' entry in the specification: "The collection
   ;; of symbol-name arguments given to the options `:shadow',
   ;; `:intern', `:import-from', and `:shadowing-import-from' must all
@@ -75,86 +41,42 @@
    (cons :import-from           (alexandria:mappend #'cdr import-from))
    (cons :shadowing-import-from (alexandria:mappend #'cdr shadowing-import-from)))
   (check-options-disjoint :intern intern :export export)
-
-  (let ((package (gensym "PACKAGE"))) ; TODO: no need for `gensym' since user code can never witness the binding
-    `(let ((,package (or (find-package ',name) ; TODO: what if NAME matches a local nickname within the current package?
-                         (make-package ',name))))
-       ;; TODO: make an `update-package' function
-       (rename-package ,package ,name '(,@nicknames))
-       ;; The order of operations is given in the specification entry
-       ;; for `defpackage':
-       ;; 1. `:shadow' and `:shadowing-import-from'
-       ,@(when (not (null shadow))
-           `((shadow '(,@shadow) ,package)))
-       ,@(loop for (from-package . names) in shadowing-import-from
-               collect `(shadowing-import (find-symbols ,from-package ,@names) ,package))
-       ;; 2. `:use'
-       ,@(when (not (null use))
-           `((use-package '(,@use) ,package)))
-       ;; 3. `:import-from' and `:intern'
-       ,@(loop for (from-package . names) in import-from
-               collect `(import (find-symbols ,from-package ,@names) ,package))
-       ,@(when (not (null intern))
-           `((intern* ,package ,@intern)))
-       ;; 4. `:export'
-       ,@(when (not (null export))
-           `((export (ensure-symbols ,package ,@export) ,package)))
-       (setf (parcl-low:documentation *client* ,package) ,documentation)
-       ,package)))
+  `(eval-when (:compile-toplevel :load-toplevel :execute)
+     (ensure-package *client* ',name
+                     ,@(loop :for (key value) :on args :by #'cddr
+                             :collect key :collect `',value))))
 
 ;;; Parsing
 
 (defun strings<-designator-asts (designator-asts)
   (mapcar #'ico:designated-string designator-asts))
 
-(defun cons<-import-from-asts (import-from-ast)
-  (let ((package-name (ico:designated-string (ico:package-name-ast import-from-ast)))
-        (symbol-names (strings<-designator-asts (ico:name-asts import-from-ast))))
-   (cons package-name symbol-names)))
-#++(defun find-symbols-form<-import-ast (import-ast)
-  )
+(defun cons<-import-from-ast (import-from-ast)
+  (let ((package-name (ico:designated-string
+                       (ico:package-name-ast import-from-ast)))
+        (symbol-names (strings<-designator-asts
+                       (ico:name-asts import-from-ast))))
+    (cons package-name symbol-names)))
 
-;;; TODO: share string designators to (potentially) safe fasl space
-(defmacro defpackage (&whole form name &rest options)
-  (declare (ignore name options))
-  (let* ((ast                   (parse-defpackage form))
-         (name                  (ico:designated-string (ico:name-ast ast)))
-         (nicknames             (strings<-designator-asts (ico:nickname-asts ast)))
-         (shadow                (strings<-designator-asts (ico:shadow-asts ast)))
-         (shadowing-import-from (mapcar #'cons<-import-from-asts
-                                        (ico:shadowing-import-from-asts ast)))
-         (use                   (strings<-designator-asts (ico:use-asts ast)))
-         (import-from           (mapcar #'cons<-import-from-asts
-                                        (ico:import-from-asts ast)))
-         (intern                (strings<-designator-asts (ico:intern-asts ast)))
-         (export                (strings<-designator-asts (ico:export-asts ast)))
-         (documentation         (alexandria:when-let ((documentation (ico:documentation-ast ast)))
-                                  (ico:%string documentation))))
-    (expand-defpackage name :nicknames             nicknames
-                            :shadow                shadow
-                            :shadowing-import-from shadowing-import-from
-                            :use                   use
-                            :import-from           import-from
-                            :intern                intern
-                            :export                export
-                            :documentation         documentation)))
+(defun conses<-import-from-asts (import-from-asts)
+  (mapcar #'cons<-import-from-ast import-from-asts))
 
-#++ (let ((*client* (make-instance 'parcl.implementation.native:client)))
-  (mapc #'delete-package '("FOO" "BAR"))
-  (describe (defpackage bar (:intern #:x "Y") (:export "BAR" "FEZ")))
-  (describe (defpackage foo
-              (:use cl "BAR")
-              (:export "BAR" :foo #:baz) (:export #\d)
-              (:intern "A" :b) (:intern #:c #\d)
-              (:shadow #:hi :yo)
-              (:shadowing-import-from #:bar #:x "Y"))))
-
-#++ (progn
-  (mapc #'cl:delete-package '("FOO" "BAR"))
-  (describe (cl:defpackage bar (:intern #:x "Y") (:export "BAR" "FEZ")))
-  (describe (cl:defpackage foo
-              (:use cl "BAR")
-              (:export "BAR" :foo #:baz) (:export #\d)
-              (:intern "A" :b) (:intern #:c #\d)
-              (:shadow #:hi :yo)
-              (:shadowing-import-from #:bar #:x "Y"))))
+;;; TODO: local-nicknames
+(define-macro defpackage (name &rest options) ast
+  (let* ((name (ico:designated-string (ico:name-ast ast)))
+         (args '()))
+    (macrolet ((maybe-initarg (initarg reader transformation)
+                 ;; TODO: we can't distinguish e.g. (:export) from no :export at all
+                 `(alexandria:when-let ((child (,reader ast)))
+                    (push (,transformation child) args)
+                    (push ,initarg                args))))
+      (maybe-initarg :nicknames     ico:nickname-asts     strings<-designator-asts)
+      (maybe-initarg :shadow        ico:shadow-asts       strings<-designator-asts)
+      (maybe-initarg :shadowing-import-from ico:shadowing-import-from-asts conses<-import-from-asts)
+      (maybe-initarg :use           ico:use-asts          strings<-designator-asts)
+      (maybe-initarg :import-from   ico:import-from-asts  conses<-import-from-asts)
+      (maybe-initarg :intern        ico:intern-asts       strings<-designator-asts)
+      (maybe-initarg :export        ico:export-asts       strings<-designator-asts)
+      (maybe-initarg :size          ico:size-ast          ico:literal)
+      (maybe-initarg :documentation ico:documentation-ast ico:%string))
+    (apply #'expand-defpackage name args )))
