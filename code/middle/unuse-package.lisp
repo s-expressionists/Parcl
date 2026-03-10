3a4a34da1699061f7f582e29001bb0df5b4ff711
Jan Moringen <jan.moringen@posteo.de>
2026-03-10 12:30:32 +0100

    middle: Clean up behavior and return value of UNUSE-PACKAGE
    * Since, in contrast to CL:UNUSE-PACKAGE, the function unuses a
      single package, a meaningful return value is possible
    
    * Use DELETE instead of REMOVE since PARCL.LOW:USE-LIST is documented
      to not return a fresh list

--- code/middle/unuse-package.lisp
+++ code/middle/unuse-package.lisp
@@ -2,13 +2,11 @@
 
 (defmethod unuse-package ((client t) (package t) (package-to-unuse t))
   (let ((use-list (low:use-list client package)))
-    #+not-in-spec (unless (member package-to-unuse use-list :test #'eq)
-      ;; TODO: recover?
-      (error 'package-is-not-used :package          package
-                                  :package-to-unuse package-to-unuse))
-    (setf (low:use-list client package)
-          (remove package-to-unuse use-list :count 1 :test #'eq))
-    (a:removef (low:used-by-list client package-to-unuse) package
-               :count 1 :test #'eq)
-    ;; TODO: return value
-    ))
+    (cond ((member package-to-unuse use-list :test #'eq)
+           (setf (low:use-list client package)
+                 (delete package-to-unuse use-list :count 1 :test #'eq))
+           (a:deletef (low:used-by-list client package-to-unuse) package
+                      :count 1 :test #'eq)
+           t)
+          (t
+           nil))))
