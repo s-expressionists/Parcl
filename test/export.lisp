e5569a1957d02a29663764dcd3e8fd8de933750d
Jan Moringen <jan.moringen@posteo.de>
2026-05-18 21:12:49 +0200

    test: Cosmetic improvements and notes

--- test/export.lisp
+++ test/export.lisp
@@ -23,6 +23,9 @@
       (parcl:export symbol package2))))
 
 (high-test export.not-accessible
+  "Ensure that an attempt to export a symbol from a package in which the
+symbol is not accessible signals the correct error and establishes the
+correct restarts."
   (mapc
    (lambda (restart-and-expected)
      (destructuring-bind (restart-name
@@ -38,6 +41,7 @@
                     (lambda (condition)
                       ;; TODO: check condition readers
                       (is-false (a:emptyp (princ-to-string condition)))
+                      ;; TODO make a function or macro for this check
                       (cond ((null restart-name)
                              (return))
                             (t
@@ -45,8 +49,10 @@
                                (is-false (a:emptyp (princ-to-string restart)))
                                (invoke-restart restart)))))))
                (parcl:export symbol package2)))
-           (is-symbol-status restart-name package1 #1# expected-export-status1 expected-shadow-status1)
-           (is-symbol-status restart-name package2 #1# expected-export-status2 expected-shadow-status2)))))
+           (is-symbol-status restart-name package1 #1#
+                             expected-export-status1 expected-shadow-status1)
+           (is-symbol-status restart-name package2 #1#
+                             expected-export-status2 expected-shadow-status2)))))
    '(;; This case simulates unwinding without any of the restart
      ;; that Parcl establishes.
      (nil                  (:internal nil) (nil       nil))
@@ -54,17 +60,22 @@
      (parcl::do-not-export (:internal nil) (nil       nil)))))
 
 ;;; TODO: can we turn this into a generic utility?
-(defun is-symbol-status (restart-name package symbol-name expected-export-status expected-shadow-status)
+(defun is-symbol-status (restart-name package symbol-name
+                         expected-export-status expected-shadow-status)
   (multiple-value-bind (actual-symbol actual-export-status)
       (parcl:find-symbol symbol-name package)
     (is (eq actual-export-status expected-export-status)
-        "~@<After ~A restart, for name ~S in package ~A, expected export status ~S but got ~S.~@:>"
-        restart-name symbol-name package expected-export-status actual-export-status)
+        "~@<After ~A restart, for name ~S in package ~A, expected export ~
+         status ~S but got ~S.~@:>"
+        restart-name symbol-name package
+        expected-export-status actual-export-status)
     (let* ((shadowing-symbols    (parcl:package-shadowing-symbols package))
            (actual-shadow-status (not (null (find actual-symbol shadowing-symbols)))))
       (is (eq actual-shadow-status expected-shadow-status)
-          "~@<After ~A restart, for name ~S in package ~A, expected shadow status ~S but got ~S.~@:>"
-          restart-name symbol-name package expected-shadow-status actual-shadow-status))))
+          "~@<After ~A restart, for name ~S in package ~A, expected shadow ~
+           status ~S but got ~S.~@:>"
+          restart-name symbol-name package
+          expected-shadow-status actual-shadow-status))))
 
 (high-test export.conflict.1
   "Ensure that `export' signals an error "
@@ -105,6 +116,9 @@
      (parcl::do-not-export (:internal nil) (:internal  nil)))))
 
 (high-test export.conflict.2
+  "Ensure that `export' signals an error and establishes a restart when
+two packages that are used by a third package both export symbols with
+identical names."
   (mapc
    (lambda (restart-and-expected)
      (destructuring-bind (restart-name
@@ -130,22 +144,26 @@
                              (return))
                             (t
                              (let ((restart (find-restart restart-name)))
-                               (is-true restart "~@<Expected to find a restart named ~S but there is none.~@:>"
+                               (is-true restart "~@<Expected to find a restart ~
+                                                 named ~S but there is none.~@:>"
                                         restart-name)
                                (is-false (a:emptyp (princ-to-string restart)))
                                (invoke-restart restart)))))))
                (parcl:export symbol2 package2)))
            ;; TODO: check the symbol object that is found for the symbol name
-           (is-symbol-status restart-name package1 #1# expected-export-status1 expected-shadow-status1)
-           (is-symbol-status restart-name package2 #1# expected-export-status2 expected-shadow-status2)
-           (is-symbol-status restart-name package3 #1# expected-export-status3 expected-shadow-status3)))))
+           (is-symbol-status restart-name package1 #1#
+                             expected-export-status1 expected-shadow-status1)
+           (is-symbol-status restart-name package2 #1#
+                             expected-export-status2 expected-shadow-status2)
+           (is-symbol-status restart-name package3 #1#
+                             expected-export-status3 expected-shadow-status3)))))
 
-   '(;; This case simulates unwinding without any of the restart
-     ;; that Parcl establishes.
+   '(;; This case simulates unwinding without any of the restart that
+     ;; Parcl establishes.
      (nil                       (:external nil) (:internal nil) (:inherited nil))
      (parcl::make-old-shadowing (:external nil) (:external nil) (:internal  t))
      (parcl::make-new-shadowing (:external nil) (:external nil) (:internal  t))
-     ; TODO: missing: "neither" aka plain cl:shadow, that is interning a new symbol that is shadowing
+     ;; TODO: missing: "neither" aka plain cl:shadow, that is interning a new symbol that is shadowing
      (parcl::do-not-export      (:external nil) (:internal nil) (:inherited nil)))))
 
 #++ (let ((package-names '("old-exporting" "new-exporting" "using")))
